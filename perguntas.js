baf9861ceed5731858746a4f2bfd1cbbdaa03f9f
leandro0080 <146841756+leandro0080@users.noreply.github.com>
2024-10-31 11:49:39 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Animais',
+    'Qual animal dorme de cabeça pra baixo?',
+    'O morcego'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital do Brasil?',
+    'Brasília'
+)
+
+criaCartao(
+    'Futebol',
+    'Quem é conhecido como rei do futebol?',
+    'Pelé'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz obrigado em Inglês?',
+    'Obrigado em ingles é thank you'
+)
